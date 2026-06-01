81d411d70e36908e5929d33dc9b2baf7883c0e8c
SAHITH MASNA <masnasahith0409@gmail.com>
2026-06-01 20:31:20 +0530

    Create 05_Move_Zeros_To_End.cpp

---
+++ Arrays/05_Move_Zeros_To_End.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 0, 2, 0, 3, 0, 4};
+    int n = 7;
+
+    int index = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[i], arr[index]);
+            index++;
+        }
+    }
+
+    cout << "Array after moving zeros: ";
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
