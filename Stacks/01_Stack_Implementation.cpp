115fb88140faf2e43ad177bebc61ab4e5e727843
SAHITH MASNA <masnasahith0409@gmail.com>
2026-06-02 22:16:48 +0530

    Create 01_Stack_Implementation.cpp

---
+++ Stacks/01_Stack_Implementation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Stack Size: " << s.size() << endl;
+
+    return 0;
+}
